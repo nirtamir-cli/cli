9a6d3cc3b4d17af922dba804d44021462ab2f9b4
Nir Tamir <nirtamir2@gmail.com>
2024-02-06 18:47:58 -0500

    eslint configuration

--- packages/commands/src/lib/integrations.ts
+++ packages/commands/src/lib/integrations.ts
@@ -121,6 +121,53 @@ export const integrations = {
 			);
 		},
 	},
+	eslint: {
+		devInstalls: [
+			"eslint",
+			"eslint-config-nirtamir2",
+		],
+		scripts: {
+			"lint": "eslint --fix \"./src/**/*.{ts,tsx,js,jsx}\"",
+		},
+		additionalConfig: async () => {
+			writeFile(
+				".eslintrc.cjs",
+				`module.exports = {
+  root: true,
+  settings: {
+    // next: {
+    //   rootDir: "src",
+    // },
+    react: {
+      version: "detect",
+    },
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  extends: [
+    "nirtamir2",
+    "nirtamir2/recommended",
+    "nirtamir2/typescript",
+    "nirtamir2/react",
+    // "nirtamir2/query",
+    // "nirtamir2/solid",
+    // "nirtamir2/security",
+    // "nirtamir2/compat",
+    // "nirtamir2/jest",
+    // "nirtamir2/storybook",
+    // "nirtamir2/i18n",
+    // "nirtamir2/query",
+    "nirtamir2/tailwindcss",
+    "nirtamir2/next", // should be after recommended react and typescript
+  ],
+  rules: {},
+};
+`,
+			);
+		},
+	},
 	// "unocss": {
 	// 	pluginOptions: {
 	// 		importName: "UnoCss",
